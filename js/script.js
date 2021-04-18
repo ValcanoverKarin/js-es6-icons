761b6977532297ef1b4947b77baf5e39c90a6870
karin valcanover <valcanoverkarin@outlook.it>
2021-04-19 00:14:55 +0200

    milestone 1 creo funzione e l 'oggetto jQuery

--- js/script.js
+++ js/script.js
@@ -103,6 +103,16 @@ const colors = [
     'purple'
 ];
 
+// ESEMPIO TEMPLATE ICONE 
+/*
+<div class="icon">
+    <i class="fas fa-cat"></i>
+    <div class="text">
+        NOME ICONA
+    </div>
+</div>
+*/
+
 // Milestone 1
 // Partendo dalla seguente struttura dati , 
 // mostriamo in pagina tutte le icone disponibili come da layout.
@@ -112,3 +122,20 @@ const colors = [
 
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+
+// Milestone 1
+
+//1b creo oggetto jQuery che rappresenti icons-container
+var iconsContainer = $('icons-container');
+//1a per scrivere tutte le icone disponibili chiamo la funzione printIcons e gli passo icons e iconsContainer
+printIcons(icons, iconsContainer);
+
+/* FUNZIONE */
+//0a. funzione che potrò richiamare piu volte. questa funzione popola un container con le icone
+//0b. ho bisogno dell array di oggetti dove ogni oggetto è un icona 
+//0c. ho bisogno del container dove stamparli
+//0d. il container dovrà essere un oggetto jQuery che rappresenta l' elemnto in cui stampare le icone
+function printIcons (iconsArray, container) {
+	//foreach per scrivere le icone nel container
+}
